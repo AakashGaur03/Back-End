5853cd969e5ddd31cecf436c1d667f3dec3fd5c3
AakashGaur03 <aakash.sunrise23@gmail.com>
2024-02-25 01:25:50 +0530

    Registration User Controller

--- Project/src/controllers/user.controller.js
+++ Project/src/controllers/user.controller.js
@@ -1,9 +1,86 @@
-import {asyncHandler} from '../utils/asyncHandler.js'
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/ApiError.js";
+import { User } from "../models/user.model.js";
+import { uploadOnCloudinary } from "../utils/cloudinary.js";
+import {ApiResponse} from "../utils/ApiResponse.js"
 
-const registerUser = asyncHandler(async (req,res) =>{
-    res.status(200).json({
-        message:"Ok"
-    })
-})
+const registerUser = asyncHandler(async (req, res) => {
+  // get user details from frontend
+  // validation - not empty
+  // check if user already exists: username,email
+  // check for images,check for avatar
+  // upload them to cloudinary,avatar
+  // create user object - create entry in db
+  // remove password and refresh token field from response
+  // check for user creation
+  // return response
 
-export {registerUser}
+  // get user details from frontend
+  const { fullName, email, username, password } = req.body;
+  // console.log(fullName,email,username,password)
+
+  // validation - not empty
+  // if(fullName===""){                                         from this we need to check all in if elseif condition
+  //     throw new ApiError(400,"Fullname is Required")
+  // }
+  if (
+    [fullName, email, username, password].some((field) => field?.trim() === "")
+  ) {
+    throw new ApiError(400, "All Fields are required");
+  }
+
+  // check if user already exists: username,email
+  const existedUser = User.findOne({
+    $or: [{ username }, { email }],
+  });
+
+  if (existedUser) {
+    throw new ApiError(409, "User with email or username already exists");
+  }
+
+  // check for images,check for avatar
+
+  // we get access of files from middleware mutler
+  const avatarLocalPath = req.files?.avatar[0]?.path;
+  const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+  if (!avatarLocalPath) {
+    throw new ApiError(400, "Avatar file is required");
+  }
+
+  // upload them to cloudinary,avatar
+  const avatar = await uploadOnCloudinary(avatarLocalPath);
+  const coverImage = await uploadOnCloudinary(coverImageLocalPath);
+
+  if (!avatar) {
+    throw new ApiError(400, "Avatar file is required");
+  }
+
+  // create user object - create entry in db
+  const user = await User.create({
+    fullName,
+    avatar: avatar.url,
+    coverImage: coverImage?.url || "",
+    email,
+    password,
+    username:username.toLowerCase()
+  });
+
+  // remove password and refresh token field from response
+  const createdUser = await User.findById(user._id).select(
+    "-password -refreshToken"                                   // it says which fields we want to remove space seprated values as a string
+  )
+
+  // check for user creation
+  if(!createdUser){
+    throw new ApiError(500,"Something went wrong while Registring the User")
+  }
+
+  // return response
+  return res.status(201).json(
+    new ApiResponse(200,createdUser,"User Registered Successfully")
+  )
+
+});
+
+export { registerUser };
